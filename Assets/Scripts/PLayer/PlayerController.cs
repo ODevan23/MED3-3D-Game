b41cd0f87e64e22014011dcb96d8dbfaafdf7488
ODevan23 <145329188+ODevan23@users.noreply.github.com>
2024-12-04 23:02:20 +0100

    Vi prøver

--- Assets/Scripts/PLayer/PlayerController.cs
+++ Assets/Scripts/PLayer/PlayerController.cs
@@ -1,16 +1,112 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class PlayerController : MonoBehaviour
+[RequireComponent(typeof(CharacterController))]
+public class FPSController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Camera playerCamera;
+    public float walkSpeed = 6f;
+    public float runSpeed = 12f;
+    public float jumpPower = 7f;
+    public float gravity = 10f;
+
+    public float lookSpeed = 2f;
+    public float lookXLimit = 45f;
+
+    private CharacterController characterController;
+    private Vector3 moveDirection = Vector3.zero;
+    private float rotationX = 0f;
+
+    private Vector2 movementInput;
+    private Vector2 lookInput;
+    private bool isJumping = false;
+    private bool isRunning = false;
+
     void Start()
     {
-        
+        characterController = GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        HandleMovement();
+        HandleCameraRotation();
+    }
+
+    private void HandleMovement()
+    {
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        Vector3 right = transform.TransformDirection(Vector3.right);
+
+        float curSpeedX = (isRunning ? runSpeed : walkSpeed) * movementInput.y;
+        float curSpeedY = (isRunning ? runSpeed : walkSpeed) * movementInput.x;
+        float movementDirectionY = moveDirection.y;
+
+        moveDirection = (forward * curSpeedX) + (right * curSpeedY);
+
+        if (characterController.isGrounded)
+        {
+            if (isJumping)
+            {
+                moveDirection.y = jumpPower;
+            }
+            else
+            {
+                moveDirection.y = 0;
+            }
+        }
+        else
+        {
+            moveDirection.y = movementDirectionY - (gravity * Time.deltaTime);
+        }
+
+        characterController.Move(moveDirection * Time.deltaTime);
+    }
+
+    private void HandleCameraRotation()
+    {
+        rotationX += -lookInput.y * lookSpeed;
+        rotationX = Mathf.Clamp(rotationX, -lookXLimit, lookXLimit);
+        playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
+
+        transform.rotation *= Quaternion.Euler(0, lookInput.x * lookSpeed, 0);
+    }
+
+    // Input System Callbacks
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        movementInput = context.ReadValue<Vector2>();
+    }
+
+    public void OnLook(InputAction.CallbackContext context)
+    {
+        lookInput = context.ReadValue<Vector2>();
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            isJumping = true;
+        }
+        else if (context.canceled)
+        {
+            isJumping = false;
+        }
+    }
+
+    public void OnRun(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            isRunning = true;
+        }
+        else if (context.canceled)
+        {
+            isRunning = false;
+        }
     }
 }
